e1417e9da8ef01a22951a57e38200c6badd6a379
Ward <wardv@tirf.ca>
2022-01-25 19:02:32 -0500

    employee test created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee.js')
+
+test('creates an Employee object', () => {
+    const employee = new Employee('Ward', '2', 'wardv@tirf.ca');
+
+    expect(employee.name).toEqual('Ward');
+    expect(employee.id).toEqual('2');
+    expect(employee.email).toEqual('wardv@tirf.ca');
+});
