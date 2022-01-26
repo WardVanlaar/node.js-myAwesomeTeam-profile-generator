abd0918f2dc660756673d54bb593908e550c46d4
Ward <wardv@tirf.ca>
2022-01-26 13:24:08 -0500

    making code DRY

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
-const Employee = require('../lib/Employee.js')
+const Employee = require("../lib/Employee.js");
 
-test('creates an Employee object', () => {
-    const employee = new Employee('Ward', '2', 'wardv@tirf.ca');
+test("creates an Employee object", () => {
+  const employee = new Employee("Ward", "2", "wardv@tirf.ca");
 
-    expect(employee.name).toEqual('Ward');
-    expect(employee.id).toEqual('2');
-    expect(employee.email).toEqual('wardv@tirf.ca');
+  expect(employee.name).toEqual("Ward");
+  expect(employee.id).toEqual("2");
+  expect(employee.email).toEqual("wardv@tirf.ca");
 });
