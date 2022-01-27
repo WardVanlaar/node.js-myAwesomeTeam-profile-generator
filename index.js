e3b66b7db63d4f1470ff5ce305462af738f89b24
Ward <wardv@tirf.ca>
2022-01-26 19:52:37 -0500

    worked on HTML page generator

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const Team = require('./lib/Team');
 
-new Team().initializeTeamManager();
+new Team().initializeTeamManager()
+  .then(teamData => {
+    return generatePage(teamData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
