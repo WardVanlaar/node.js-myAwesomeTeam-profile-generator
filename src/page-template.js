1fe381ec45fcd499b3ff1fbd0072bab9e7242eba
Ward <wardv@tirf.ca>
2022-01-28 14:14:51 -0500

    fixed some bugs and some styling

--- src/page-template.js
+++ src/page-template.js
@@ -1,14 +1,16 @@
+// code adapted from https://github.com/nicolewallace09/team-profile-generator
+
 // create Manager card
 const generateManager = function (manager) {
   return `
   <div class="col-4 mt-4">
       <div class="card h-100">
-          <div class="card-header">
+          <div class="card-header" style="background-color: #898ca8">
               <h3>${manager.name}</h3>
-              <h4>Manager</h4><i class="material-icons">content_paste</i>
+              <h4>Manager</h4><i class="material-icons">manage_accounts</i>
           </div>
 
-          <div class="card-body">
+          <div class="card-body" style="background-color: #dedfe7">
               <p class="id">ID: ${manager.id}</p>
               <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
               <p class="office">Office Number: ${manager.officeNumber}</p>
@@ -24,15 +26,15 @@ const generateEngineer = function (engineer) {
   return `
   <div class="col-4 mt-4">
       <div class="card h-100">
-          <div class="card-header">
+          <div class="card-header" style="background-color: #898ca8">
               <h3>${engineer.name}</h3>
-              <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+              <h4>Engineer</h4><i class="material-icons">engineering</i>
           </div>
 
-          <div class="card-body">
+          <div class="card-body" style="background-color: #dedfe7">
               <p class="id">ID: ${engineer.id}</p>
               <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-              <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+              <p class="github">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
           </div>
 
       </div>
@@ -45,12 +47,12 @@ const generateIntern = function (intern) {
   return `
   <div class="col-4 mt-4">
       <div class="card h-100">
-          <div class="card-header">
+          <div class="card-header" style="background-color: #898ca8">
               <h3>${intern.name}</h3>
-              <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+              <h4>Intern</h4><i class="material-icons">perm_identity</i>
           </div>
 
-          <div class="card-body">
+          <div class="card-body" style="background-color: #dedfe7">
               <p class="id">ID: ${intern.id}</p>
               <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
               <p class="school">School: ${intern.school}</p>
@@ -120,7 +122,7 @@ return`
 </head>
 <body>
     <header>
-        <nav class="navbar" id="navbar">
+        <nav class="navbar navbar-light" style="background-color: #898ca8" id="navbar">
             <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
         </nav>
     </header>
